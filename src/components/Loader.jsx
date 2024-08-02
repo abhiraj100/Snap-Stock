7c361ae0416d7a63cad0571b2e918504a2e4bc8a
Abhiraj Yadav <abhiraj100right@gmail.com>
2024-08-02 15:46:59 +0530

    snap-stock react project

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import spinner from '../spinner.gif'
+
+const Loader = () => {
+  return (
+    <>
+      <div className="img">
+        <img src={spinner} alt="LOADING" />
+      </div>
+
+    </>
+  )
+}
+
+export default Loader
